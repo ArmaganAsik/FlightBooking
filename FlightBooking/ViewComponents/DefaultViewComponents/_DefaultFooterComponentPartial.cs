20cd7786aa16b27b50604c92d9c22de5de7f8ce3
Armağan AŞIK <140605164+ArmaganAsik@users.noreply.github.com>
2026-03-23 00:22:47 +0300

    Add project files.

---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultFooterComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultFooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
